35f23de2525f4e6b96ed70340e7756d25b13d9de
Mark Tobler <marktobler@gmail.com>
2021-03-04 16:47:17 -0800

    clickable controls

--- sb.js
+++ sb.js
@@ -53,14 +53,86 @@ document.addEventListener(
             'click',
             goNext,
             false
-      );
+         );
 
       document.querySelector('#prev')
          .addEventListener(
             'click',
             goPrev,
             false
+         );
+
+      document.querySelector('#image')
+         .addEventListener(
+            'click',
+            printPosition,
+            false
       );
+      
+      // let's create the overlays
+      let fLDiv = document.createElement("div");
+      let leftNode = document.createTextNode("<");
+      fLDiv.appendChild(leftNode);
+      fLDiv.style.float = "left";
+      fLDiv.style.position = "fixed";
+      fLDiv.style.left = "8rem";
+      fLDiv.style.top = "16rem";
+      fLDiv.style.fontSize = "6.5rem";
+      fLDiv.style.color = "rgba(20,20,20,.05)";
+      fLDiv.id = "leftC";
+      fLDiv.addEventListener("click", goPrev);
+      document.getElementById("image").appendChild(fLDiv);
+
+      let fRDiv = document.createElement("div");
+      let rightNode = document.createTextNode(">");
+      fRDiv.appendChild(rightNode);
+      fRDiv.style.float = "right";
+      fRDiv.style.position = "fixed";
+      fRDiv.style.left = "33rem";
+      fRDiv.style.top = "16rem";
+      fRDiv.style.fontSize = "6.5rem";
+      fRDiv.style.color = "rgba(20,20,20,.05)";
+      fRDiv.id = "rightC";
+      fRDiv.addEventListener("click", goNext);
+      document.getElementById("image").appendChild(fRDiv);
+
+
+
+
+
 
    }
 )
+
+function printPosition(e) {
+   // console.log(getPosition(e));
+   let xy = getPosition(e.currentTarget);
+   console.log(xy);
+   console.log('xx', xy.x, 'yy', xy.y);
+}   
+// helper function to get an element's exact position
+function getPosition(el) {
+   var xPosition = 0;
+   var yPosition = 0;
+  
+   while (el) {
+     if (el.tagName == "BODY") {
+       // deal with browser quirks with body/window/document and page scroll
+       var xScrollPos = el.scrollLeft || document.documentElement.scrollLeft;
+       var yScrollPos = el.scrollTop || document.documentElement.scrollTop;
+  
+       xPosition += (el.offsetLeft - xScrollPos + el.clientLeft);
+       yPosition += (el.offsetTop - yScrollPos + el.clientTop);
+     } else {
+       xPosition += (el.offsetLeft - el.scrollLeft + el.clientLeft);
+       yPosition += (el.offsetTop - el.scrollTop + el.clientTop);
+     }
+  
+     el = el.offsetParent;
+   }
+   console.log('gp: xpos:', xPosition);
+   return {
+     x: xPosition,
+     y: yPosition
+   };
+ }
